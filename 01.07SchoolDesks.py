cc8fc82fd5ed47542bd9fdb677705e0c01f2166b
GAGEUALR <glnieffenegg@ualr.edu>
2022-01-29 19:18:37 +0000

    commit

---
+++ 01.07SchoolDesks.py
@@ -0,0 +1,9 @@
+classa = int((input("Enter Classroom A: ")))
+classb = int((input("Enter Classroom B: ")))
+classc = int((input("Enter Classroom C: ")))
+
+numberofdesksa = int(classa // 2)
+numberofdesksb = int(classb // 2)
+numberofdesksc = int(classc // 2)
+
+print(str(numberofdesksa + numberofdesksb + numberofdesksc))
