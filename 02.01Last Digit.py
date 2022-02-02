424cce248505d53cb9bc4190f6836cbc4bcca720
GAGEUALR <glnieffenegg@ualr.edu>
2022-02-02 15:35:04 +0000

    commit

---
+++ 02.01Last Digit.py
@@ -0,0 +1,8 @@
+x = int(input("Enter a Number: "))
+y = int(10)
+
+z = str(x % y)
+
+print(z)
+
+
---
+++ 03.01 Minimum of Three Numbers.py
@@ -0,0 +1,9 @@
+x = input("Enter First Number: ")
+y = input("Enter Second Number: ")
+z = input("Enter Third Number: ")
+
+if x <= y + x <= z: print(x)
+
+else y <= z + y <= x: print(y)
+
+ z <= x + z <= y: print(z)
