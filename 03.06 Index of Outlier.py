475b84d39dd08adba29f27383a9851f027696071
GAGEUALR <glnieffenegg@ualr.edu>
2022-02-09 06:10:58 +0000

    commit

---
+++ 03.06 Index of Outlier.py
@@ -0,0 +1,12 @@
+a = input("Enter First Number: ")
+b = input("Enter Second Number: ")
+c = input("Enter Third Number: ")
+
+if b == c:
+    print(1)
+
+elif a == b:
+    print(3)
+
+elif c == a:
+    print(2)
