1be016ca890f7339542e91c6b22311d889bb3aa5
GAGEUALR <glnieffenegg@ualr.edu>
2022-02-15 20:13:48 +0000

    commit

--- 04.04NumberofZeros.py
+++ 04.04NumberofZeros.py
@@ -1,11 +1,13 @@
 N = int(input("Enter N: "))
 x = 0
-for i in range (1, N + 1):
-    b = input("Enter a Number: ")
-    if b == 0:
-        x += 1
-        
-print(x)
+y = 0
+for x in range (1, N + 1):
+    b = int(input("Enter a Number: "))
+    if b == int(0):
+        y += 1 
+     
+
+print(y)
 
 
 
