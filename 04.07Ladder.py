be9024fe80dda4d061f11aa1c6207c5f418f7b38
GAGEUALR <glnieffenegg@ualr.edu>
2022-02-15 21:06:28 +0000

    commit

---
+++ 04.07Ladder.py
@@ -0,0 +1,40 @@
+N = int(input("Enter N: "))
+a = 1
+b = 1,2
+c = 1,2,3
+d = 1,2,3,4
+e = 1,2,3,4,5
+f = 1,2,3,4,5,6
+g = 1,2,3,4,5,6,7
+h = 1,2,3,4,5,6,7,8
+i = 1,2,3,4,5,6,7,8,9
+if N > 9:
+    print("INVALID")
+
+    for N in range(1):
+         print(a)
+
+    for N in range(2):
+         print(b,sep="")
+    
+    for N in range(3):
+         print(c,sep="")
+
+    for N in range(4):
+         print(d, sep="")
+
+    for N in range(5):
+         print(e,sep="")
+
+    for N in range(6):
+         print(f,sep="")
+
+    for N in range(7):
+         print(g,sep="")
+
+    for N in range(8):
+         print(h,sep="")
+
+    for N in range(9):
+         print(i,sep="")
+         
