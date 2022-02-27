9899192dcd99e68321fc7aeb16bb67107ce5d21a
GAGEUALR <glnieffenegg@ualr.edu>
2022-02-27 22:47:51 +0000

    commit

--- 06.01Circle.py
+++ 06.01Circle.py
@@ -1,24 +1,13 @@
-radius = open("06.01radius.py", "r")
+radiusfile = open("06.01radius.txt", "r")
+x = radiusfile.readline()
 
-dia = 2
+def diameter(x):
+    while x != "":
+        r = radiusfile.readline()
+        diameterr = (r * 2)
+        return diameter
 
-def diameter(r, dia):
-    r ** dia 
-
-def circumfrence(r, circ):
-    dia ** r ** 3.1415
-
-def area(r):
-    3.1415 ** r ** r
-
-r = radius.read()
-
-print(diameter)
-print(circumfrence)
-print(area)
+print(diameter(x))
 
 
-
-
-        
 
