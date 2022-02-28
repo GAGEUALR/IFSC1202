e1e62feb847a804ac557a788e706f3a3e9169478
GAGEUALR <glnieffenegg@ualr.edu>
2022-02-28 16:19:55 +0000

    commit

--- 06.01Circle.py
+++ 06.01Circle.py
@@ -1,13 +1,25 @@
+from math import pi
+
+def diameter(x):
+        b = (x * 2)
+        return b
+
+def circumference(x):
+    c = (2 * pi * x)
+    return c
+
+def area(x):
+    d = (pi * x ** 2)
+    return d 
+
+print("{:>15s} {:>15s} {:>15s} {:>15s}".format("Radius","Diameter","Circumference","Area"))
 radiusfile = open("06.01radius.txt", "r")
 x = radiusfile.readline()
 
-def diameter(x):
-    while x != "":
-        r = radiusfile.readline()
-        diameterr = (r * 2)
-        return diameter
-
-print(diameter(x))
-
+while x != "":
+    x = float(x)
+    print("{:15.5f} {:15.5f} {:15.5f} {:15.5f}".format(x, diameter(x), circumference(x), area(x)))
+    x = radiusfile.readline()
+radiusfile.close()
 
 
---
+++ 06.02 Stock.py
@@ -0,0 +1,2 @@
+def percentdiff(x):
+    a = 
