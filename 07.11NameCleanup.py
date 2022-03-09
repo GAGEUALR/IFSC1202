ecc742cd2f0c31cbc49a5aa026dace0ec7462448
GAGEUALR <glnieffenegg@ualr.edu>
2022-03-09 15:58:51 +0000

    commit

--- 07.11NameCleanup.py
+++ 07.11NameCleanup.py
@@ -1,34 +1,49 @@
 def propercase(s):
-    s.upper(s[0])
-    s.lower(s[1:-1])
+    if len(s) == 0:
+        return s
+    s = s.lower()
+    s = s[0].upper() + s[1:]
+    return s
 
 def RemoveNewLine(s):
-     s.replace("\n", "")
+     s = s.replace("\n", "")
+     return s
 
 def trim(s):
-    s.strip(s)
+    s = s.strip()
+    return s
 
-def Firstname(s, x):
+def Firstname(s):
+    s = trim(s)
     x = (s.find(" "))
-    y = str(s[0:x])
-    y.propercase(y[0:x])
-    return y
+    y = (s[0:x])
+    return propercase(y[0:x])
 
 def Lastname(s):
+    s = trim(s)
     y = s.rfind(" ")
-    s.propercase(s[y:-1])
+    z = (s[y+1:])
+    return propercase(z)
 
 def Middlename(s):
+    s = trim(s)
     x = s.find(" ")
     y = s.rfind(" ")
-    s.propercase()
+    z = (s[x:y])
+    a = trim(z)
+    b = propercase(a)
+    if len(b) == 1:
+        b = b + "."
+    return b
 
 
 a = open("Names.txt", "r")
 s = a.readline()
-x = s.find(" ")
-y = s.rfind(" ")
+
+
+print("{:10} {:10} {:10} {}".format("First", "Middle", "Last", "\n----------------------------"))
+
 while s != "":
     s = str(s)
-    print("{} {} {} {}".format(s, Firstname(s, x), Middlename(s), Lastname(s)))
+    print("{:10s} {:10s} {:10s}".format(Firstname(s), Middlename(s), Lastname(s)))
     s = a.readline()
