c070218214ae44bcb41139ac4fe5939fec0dcb89
GAGEUALR <glnieffenegg@ualr.edu>
2022-03-16 15:16:33 +0000

    commit

--- 08.11USPopulation.py
+++ 08.11USPopulation.py
@@ -1,33 +1,43 @@
-popfile = open("08.11USPopulation.txt", "r")
-
-def change(x):   #not sure how to get the readfile function to go backwards or save the last readline in a variable ?,
-        for i in range(x):
-                if x =="":
-                        j = print("N/A")
-                if x != "":
-                        j = x - x[i - 1]
-                if x[i - 1] == "": 
-                 j = print("N/A")
-        return j
 
 print("{:<15} {:<15} {:<15} {:<15}".format("Year", "Population", "Change", "Percent Change"))
 
-year = 1950
 b = []
-
-while year != 1991:
-    for i in range(0, 41):
-        b.append(year)
-        year += 1
-
+popfile = open("08.11USPopulation.txt", "r")
 x = popfile.readline()
-x = (int(x) * 1000)
-
-for i in range(0, 41):
-        print("{:<15} {:<15}".format(b[i], x,)) #{:<15} ,change(x) 
-        x = []
+while x != '':
+        x = (int(x) * 1000)
+        b.append(x)
         x = popfile.readline()
-        if x == "":
-                break
-        x = (int(x))
-        x *= 1000
+popfile.close()
+
+year = 1950
+print("{:<15d} {:<15d} {:<15s} {:<15s}".format(year, b[0], "N/A", "N/A")) 
+
+minchange = b[1] - b[0]
+minyear = year
+
+maxchange = b[1] - b[0]
+maxyear = year
+
+
+year = year + 1
+
+for i in range(1, len(b)):
+        change = b[i] - b[i-1]
+        percentchange = (((change)/b[i - 1])* 100)
+        print("{:<15} {:<15} {:<15d} {:>15.2f}%".format(year, b[i], change, percentchange))
+        if b[i] - b[i-1] < minchange:
+                minchange = b[i]-b[i-1]
+                minyear = year
+        year = year + 1
+        if b[i] - b[i - 1] > maxchange:
+                maxchange = b[i]-b[i -1]
+                maxyear = year
+        #if i > 1:
+                #avchange = ((percentchange) + (percentchange(b[i - 1])))/i
+
+print("\nMinimum Change = ", minchange, "({})".format(minyear))
+
+print("Maximum Change = ", maxchange,"({})".format(maxyear))
+
+# couldn't figure out avchange, kept getting "can't call float" error.
