4294e9b265c2511cb1bbe9b8cac9f15c16ee9998
GAGEUALR <glnieffenegg@ualr.edu>
2022-04-11 22:23:51 +0000

    commit

--- 11.01RetailItem.py
+++ 11.01RetailItem.py
@@ -5,4 +5,16 @@ class RetailItem ():
         self.unitsonhand = unitsonhand
         self.price = price
 
-    
+    def InventoryValue(self):
+        InventoryValue = (float(self.Price) * float(self.UnitsonHand))
+        return InventoryValue
+
+def print_itemlist(itemlist):
+	print("{:>14s}{:>14s}{:>14s}{:>14s}".format("Type", "Diameter", "Pressure", "Circumference"))
+	for i in range(len(itemlist)):
+		print ("{:>14s}{:14.2f}{:14.2f}{:14.2f}".format(itemlist[i].itemType, itemlist[i].Diameter, itemlist[i].Pressure, itemlist[i].Circumference()))
+
+def find_item(itemlist, itemtofind):
+	for i in range(len(itemlist)):
+		if itemlist[i].itemType == itemtofind:
+			return i
