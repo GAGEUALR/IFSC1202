f48f2b42d4e945accfc621f3fa85b4eb39ba8fd4
GAGEUALR <glnieffenegg@ualr.edu>
2022-04-17 00:02:29 +0000

    commit

--- 11.02Payroll.py
+++ 11.02Payroll.py
@@ -7,39 +7,54 @@ class Employee ():
         self.hoursworked = hoursworked
         self.wage = wage
         
-def weeklypay(self):
-             hoursworked = float(employee1.hoursworked)
-             wage = float(employee1.wage)
-             if hoursworked <= 40:
-                totalpay = (hoursworked * wage)
-                return totalpay
-             if hoursworked > 40:
-                overhours = (hoursworked - 40)
-                totalpay = (overhours * wage * 1.5) + ((hoursworked - overhours) * wage)
-                return totalpay
+    def weeklypay(self):
+        hoursworked = float(self.hoursworked)
+        wage = float(self.wage)
+        if hoursworked <= 40:
+           totalpay = (hoursworked * wage)
+           return totalpay
+        if hoursworked > 40:
+            overhours = (hoursworked - 40)
+            totalpay = (overhours * wage * 1.5) + ((hoursworked - overhours) * wage)
+            return float(totalpay)
+
+    def changehours(self, newhoursworked):
+        self.hoursworked = float(0)
+        self.hoursworked = newhoursworked
+        return self.hoursworked
 
 def find_employee(employeelist, employeetofind):
 	for i in range(len(employeelist)):
-		if employeelist[i].firstname == employeetofind:
+		if employeelist[i].IDnumber == ysplit[0]:
 			return i
             
 
 def print_employeelist(employeelist):
-	print("{:<30}{:<30}{:<30}{:<30}".format("First Name", "Last Name", "ID Number", "Hours Worked"))
+	print("{:<20}{:<20}{:<20}{:<20}{:<20}{:<20}".format("First Name", "Last Name", "ID Number", "Hours Worked", "Hourly Wage", "Weekly Pay"))
 	for i in range(len(employeelist)):
-		print ("{:<30}{:<30}{:<30.2f}{:<30.2f}{:<30.2f}".format(employeelist[i].firstname, employeelist[i].lastname, employeelist[i].IDnumber, employeelist[i].wage, weeklypay()))
+	    print ("{:<20}{:<20}{:<20}{:<20}{:<20}{:<20.2f}".format(employeelist[i].firstname, employeelist[i].lastname, employeelist[i].IDnumber, employeelist[i].hoursworked.replace("\n", ""), employeelist[i].wage.replace("\n", ""), employeelist[i].weeklypay()))
 
 employeelist = []
 
 employeefile = open("11.02Employees.txt", "r")
 x = employeefile.readline()
+hoursupdate = open("11.02Hours.txt", "r")
+y = hoursupdate.readline()
 
 while x != "":
     xsplit = x.split(",")
-    employee1 = Employee(xsplit[0], (xsplit[1]), (xsplit[2]), xsplit[3])
+    employee1 = Employee(xsplit[0], (xsplit[1]), (xsplit[2]), float(0), xsplit[3])
     employeelist.append(employee1)
     x = employeefile.readline()
 
+while y != "":
+    ysplit = y.split(",")
+    newhoursworked = ysplit[1]
+    employeelist[find_employee(employeelist, ysplit[0])].changehours(newhoursworked)
+    y = hoursupdate.readline()
+
 
 print_employeelist(employeelist)
-    
+
+employeefile.close()
+hoursupdate.close()
